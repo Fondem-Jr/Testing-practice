e20c509a50583f2c38ac21eac59737cd57b5d133
Fondem Junior <fondemjunior@yahoo.com>
2021-11-03 18:17:54 +0100

    Add simple calculator class

--- calculator.js
+++ calculator.js
@@ -1,18 +1,22 @@
 class calculator {
-    constructor(a, b){
-        this.a = a;
-        this.b = b;
+    constructor(num1, num2) {
+        this.num1 = num1;
+        this.num2 = num2;
     }
-    add(){
-        return this.a + this.b;
+    add() {
+        return this.num1 + this.num2;
     }
-    substract(){
-        return this.a - this.b;
-    }
-    divide(){
-        return this.a / this.b;
-    }
-    multiply(){
-        return this.a * this.b;
-    }
+    substract() {
+        return this.num1 - this.num2;
+    };
+    divide() {
+        return this.num1 / this.num2;
+    };
+    multiply() {
+        return this.num1 * this.num2;
+    };
+    
 }
+
+const calculate = new calculator(2, 3);
+console.log(calculate.add());
